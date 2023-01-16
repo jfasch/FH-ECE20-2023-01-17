b195894a8cd2749d58db1a642b72a8c6daf43e8a
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-01-16 14:07:52 +0100

    initial commit

--- code/filter.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <vector>
-
-std::vector<int> filter(const std::vector<int>& input, bool(*)(int));
--- code/uniq.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <vector>
-
-std::vector<int> uniq(const std::vector<int>& input);
